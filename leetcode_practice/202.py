8587f0a304adf81da9d851bbef3328cce3c465c7
YY <yangyuebfsu@gmail.com>
2020-03-12 19:34:44 -0700

    202 happy number

---
+++ leetcode_practice/202.py
@@ -0,0 +1,32 @@
+***
+202. Happy Number
+
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+***
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        output=[]
+        num=n
+        while True:
+            temp= sum( int(i)*int(i) for i in str(num)  ) 
+            if temp ==1 :
+                return True
+            elif temp in output:
+                return False
+            elif temp not in output:
+                output.append(temp)
+            num=temp
