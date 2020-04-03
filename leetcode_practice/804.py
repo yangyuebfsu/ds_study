1938e8ecfd76444c8fa5dc0a63feebe65d9a7253
YY <yangyuebfsu@gmail.com>
2020-04-02 17:36:56 -0700

    804 Unique Morse Code Words

---
+++ leetcode_practice/804.py
@@ -0,0 +1,37 @@
+***
+804. Unique Morse Code Words
+
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+
+***
+
+import string
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        s_to_code = dict(zip(string.ascii_lowercase, [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]))
+        output_dict={}
+        for word in words:
+            temp_morse=''
+            for letter in list(word):
+                temp_morse=temp_morse+s_to_code[letter]
+            output_dict[temp_morse]=0
+        return len(output_dict.keys())
