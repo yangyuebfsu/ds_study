24a6546ee95e4bb61102d398d1d8a55bec560cd6
YY <yangyuebfsu@gmail.com>
2020-06-15 19:27:16 -0700

    628. Maximum Product of Three Numbers

---
+++ leetcode_practice/628.py
@@ -0,0 +1,21 @@
+***
+628. Maximum Product of Three Numbers
+
+Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+Example 1:
+
+Input: [1,2,3]
+Output: 6
+ 
+
+Example 2:
+
+Input: [1,2,3,4]
+Output: 24
+***
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return max( nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[-1])
+
