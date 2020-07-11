98076b59ff6d493235685df7d49fa6f9bef1e2b1
YY <yangyuebfsu@gmail.com>
2020-07-10 20:49:02 -0700

    234. Palindrome Linked List

---
+++ leetcode_practice/234.py
@@ -0,0 +1,30 @@
+***
+234. Palindrome Linked List
+
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+***
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        output = []
+        while head:
+            output.append(head.val)
+            head = head.next
+
+        return output == list( reversed(output) )
+        
+            
