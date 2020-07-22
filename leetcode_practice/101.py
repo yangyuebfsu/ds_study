d99267a6e02a1fe10fa834c6754661276063298a
yangyuebfsu <yangyuebfsu@gmail.com>
2020-07-21 20:02:38 -0400

    101. Symmetric Tree

---
+++ leetcode_practice/101.py
@@ -0,0 +1,37 @@
+'''101. Symmetric Tree
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3'''
+   
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        def check(l,r):
+            if l and r and l.val==r.val:
+                return check(l.left,r.right) and check(r.left,l.right)
+            return l==r
+        return check(root.left,root.right)
+        
